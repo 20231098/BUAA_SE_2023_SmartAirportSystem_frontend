4d48f2ea17e49e211b18f321fc3c1a700d4cff86
Zora <qy031110@gmail.com>
2023-05-24 22:44:40 +0800

    更新页面

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,16 @@
 <template>
-  <div class="routers">
+  <div class="routers" ref="action">
     <router-view></router-view>
-    <router-link to="/" class="router1">首页</router-link>
-    <router-link to="/login" class="router2">登录/注册</router-link>
+    <!-- <router-link to="/" class="router1">首页</router-link>
+    <router-link to="/login" class="router2">登录/注册</router-link> -->
   </div>
+  <!-- <div>hello</div> -->
 </template>
-<script>
 
+<script>
 
 </script>
 
 <style>
-.routers {
-  z-index: 9999;
-}
 
-.router1 {
-  padding: 10px;
-}
-
-.router2 {
-  padding: 5px;
-}
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,9 +5,18 @@
         <h1 v-else-if="time == 2">Good Afternoon</h1>
         <h1 v-else>Good Evening</h1>
     </div>
+
+    <div class="box">
+        <a class="items" v-for="(item, index) in list1" :key="index" onmouseover="this.style.color = 'blue'"
+            onmouseout="this.style.color = 'black'" @click="changePage(item)">
+            {{ item.name }}
+        </a>
+    </div>
 </template>
 
 <script>
+import router from '@/routes/router';
+
 export default {
     data() {
         var ret = 1;
@@ -20,8 +29,27 @@ export default {
         } else {
             ret = 3;
         }
+
         return {
+            list1: [
+                {name:"首页", index:1},
+                {name:"查询", index:2},
+                {name:"登录/注册", index:3},
+            ],
+
+            boxStyle: {
+
+            },
+
             time: ret
+        }
+    },
+
+    methods: {
+        changePage(item){
+            if(item.index == 3){
+                router.push('/login');
+            }
         }
     }
 
@@ -35,18 +63,36 @@ export default {
     position: absolute;
     width: 100%;
     height: 100%;
-    background-color:rgb(225, 239, 255);
+    background-color: rgb(225, 239, 255);
     z-index: -99999;
 }
 
-*{
+* {
     margin: 0;
     padding: 0;
 }
 
 .greeting {
     margin: 10px;
+    padding: 10px;
     position: absolute;
     top: 30px;
+}
+
+.items {
+    display: inline-block;
+    height: 41px;
+    text-decoration: none;
+    color: #4c4c4c;
+    padding: 0 20px;
+}
+
+.box {
+    height: 45px;
+    border-top: 3px solid blue;
+    border-bottom: 1px solid grey;
+    background-color: white;
+    font-size: 15px;
+    line-height: 41px;
 }
 </style>
