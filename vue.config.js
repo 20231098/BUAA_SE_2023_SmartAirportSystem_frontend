32794c730a641b07c91b7bcdc1742db2ac377b85
Boat1098 <20231098@buaa.edu.cn>
2023-06-06 23:22:48 +0800

    进一步完善

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer: {
+      proxy: {
+          '/api': {
+              target: 'https://test.com/',//这里填入你要请求的接口的前缀
+              ws:true,//代理websocked
+              changeOrigin:true,//虚拟的站点需要更管origin
+              secure: true,                   //是否https接口
+              pathRewrite:{
+                  '^/api':''//重写路径
+              }
+          }
+      }
+  }
+}
